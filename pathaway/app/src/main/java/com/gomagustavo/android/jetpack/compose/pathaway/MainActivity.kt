42765115a77b1db619b89c767833730feb4c78b9
Gustavo Araújo <gustavo.hg.araujo@gmail.com>
2022-07-08 11:47:32 -0300

    Creating project

---
+++ pathaway/app/src/main/java/com/gomagustavo/android/jetpack/compose/pathaway/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.gomagustavo.android.jetpack.compose.pathaway
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            // UI
+        }
+    }
+}
