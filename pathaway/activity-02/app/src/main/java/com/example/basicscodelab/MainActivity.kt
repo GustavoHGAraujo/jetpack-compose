e5d978e935c662acd802098a555741b449c10a5b
Gustavo Araújo <gustavo.hg.araujo@gmail.com>
2022-07-10 16:35:44 -0300

    States in Compose

--- pathaway/activity-02/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ pathaway/activity-02/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,22 +42,27 @@ private fun MyApp(
 
 @Composable
 private fun Greeting(name: String) {
+
     Surface(
         color = MaterialTheme.colors.primary,
-        modifier = Modifier
-            .padding(vertical = 4.dp, horizontal = 8.dp)
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
+        val expanded = remember { mutableStateOf(false) }
+
         Row(Modifier.padding(24.dp)) {
             Column(
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = if (expanded.value) 48.dp else 0.dp)
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
+
             OutlinedButton(
-                onClick = { /* TODO */ }
+                onClick = { expanded.value = expanded.value.not() }
             ) {
-                Text("Show more")
+                Text(text = if (expanded.value) "Show less" else "Show more")
             }
         }
     }
